1c5049654997efab6db303b522c7ba26ee98337b
skyline-blue <2895054643@qq.com>
2022-11-08 18:06:15 +0800

    update: multi-thread test

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -1,6 +1,9 @@
 import com.skyline.command.CommandUtil;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * [FEATURE INFO]<br/>
@@ -19,6 +22,18 @@ public class TestMain {
         CommandUtil.enable();
         Thread thread = new Thread(new ConsoleIOListener());
         thread.start();
+
+        AtomicInteger integer = new AtomicInteger(0);
+
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            Thread thread_ = new Thread(() -> CommandUtil.dispatchToCache("plugin load -d " + integer.incrementAndGet()));
+
+            threads.add(thread_);
+        }
+
+        threads.forEach(Thread::start);
+
     }
 
 }
